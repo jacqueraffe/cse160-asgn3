b6167f2d72d14bd9600c4fcc86a0dadd816c7120
Jacqueline Palevich <jacqueline.palevich@gmail.com>
2025-03-06 23:10:31 -0800

    draw map

--- src/asgn3.js
+++ src/asgn3.js
@@ -199,6 +199,30 @@ function tick() {
 var g_camera = new Camera();
 g_camera.updateViewMatrix();
 
+var g_map = [
+  [1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 1, 1, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 1, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 1],
+  ];
+  
+  function drawMap() {
+    for (x=0;x<8;x++) {
+      for (y=0;y<8;y++) {
+        if (g_map[x][y]==1){
+          var body = new Cube();
+          body.color = [1.0,1.0,1.0,1.0];
+          body.matrix.translate(x-4, -.75, y-4);
+          body.render();
+        }
+      }
+    }
+  }
+  
 function renderAllShapes(){
   // Clear <canvas>
   var startTime = performance.now();
@@ -219,7 +243,7 @@ function renderAllShapes(){
   //gl.enable(gl.CULL_FACE);
   //gl.cullFace(gl.BACK);
   gl.enable(gl.DEPTH_TEST);
-  
+  drawMap();
   var floor = new Cube();
   floor.color = [10/256, 200/255, 10/255, 1.0];
   floor.matrix.translate(0, -0.75, 0);
