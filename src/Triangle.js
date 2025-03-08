4e07062d2f41dc99cd8fa1b894b1850cb01d9f9b
Jacqueline Palevich <jacqueline.palevich@gmail.com>
2025-03-08 11:07:09 -0800

    so many frames!

--- src/Triangle.js
+++ src/Triangle.js
@@ -32,31 +32,43 @@ function drawTriangle3D(vertices) {
     gl.enableVertexAttribArray(a_Position);
  
     gl.drawArrays(gl.TRIANGLES, 0, n);
+    // so that drawTriangle3DUV can set stuff up again
+    g_vertexBuffer = null;
  }
+ 
+ var g_vertexBuffer=null;
+ var g_uvBuffer=null;
+ 
+function initTriangle3DUV() {
+  g_vertexBuffer = gl.createBuffer();
+  if (!g_vertexBuffer) {
+    console.log('Failed to create the buffer object');
+    return -1;
+  }
+  gl.bindBuffer(gl.ARRAY_BUFFER, g_vertexBuffer);
+  gl.vertexAttribPointer(a_Position, 3, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(a_Position);
+  
+  g_uvBuffer = gl.createBuffer();
+    if (!g_uvBuffer) {
+        console.log('Failed to create the buffer object');
+        return -1;
+    }
+    gl.bindBuffer(gl.ARRAY_BUFFER, g_uvBuffer);
+    gl.vertexAttribPointer(a_UV, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(a_UV);
+}
  
  
 function drawTriangle3DUV(vertices, uv) {
     var n = vertices.length/3;
-    
-    var vertexBuffer = gl.createBuffer();
-    if (!vertexBuffer) {
-        console.log('Failed to create the buffer object');
-        return -1;
+    if (g_vertexBuffer == null){
+        initTriangle3DUV();
     }
-    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    gl.bindBuffer(gl.ARRAY_BUFFER, g_vertexBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.DYNAMIC_DRAW);
-    gl.vertexAttribPointer(a_Position, 3, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(a_Position);
-    
-    var uvBuffer = gl.createBuffer();
-    if (!uvBuffer) {
-        console.log('Failed to create the buffer object');
-        return -1;
-    }
-    gl.bindBuffer(gl.ARRAY_BUFFER, uvBuffer);
+    gl.bindBuffer(gl.ARRAY_BUFFER, g_uvBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(uv), gl.DYNAMIC_DRAW);
-    gl.vertexAttribPointer(a_UV, 2, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(a_UV);
- 
     gl.drawArrays(gl.TRIANGLES, 0, n);
+
  }
