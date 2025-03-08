546ec66be347c2721d4d631d467c6f560c0a3c0d
Jacqueline Palevich <jacqueline.palevich@gmail.com>
2025-03-08 11:30:42 -0800

    so effiecnt!

--- src/Cube.js
+++ src/Cube.js
@@ -4,6 +4,48 @@ class Cube {
        this.color = [1.0, 1.0, 1.0, 1.0];
        this.matrix = new Matrix4();
        this.textureNum = 0;
+    
+       this.cubeVertsXYZ = [
+        // xy0 face
+        0.0, 0.0, 0.0,   1.0, 1.0, 0.0,   0.0, 1.0, 0.0,
+        0.0, 0.0, 0.0,   1.0, 0.0, 0.0,   1.0, 1.0, 0.0,
+        // xy1
+        1.0, 1.0, 1.0,   0.0, 0.0, 1.0,   0.0, 1.0, 1.0,
+        1.0, 0.0, 1.0,   0.0, 0.0, 1.0,   1.0, 1.0, 1.0,
+        // 0yz
+        0.0, 1.0, 1.0,   0.0, 0.0, 0.0,   0.0, 1.0, 0.0,
+        0.0, 0.0, 1.0,   0.0, 0.0, 0.0,   0.0, 1.0, 1.0,
+        // 1yz
+        1.0, 0.0, 0.0,   1.0, 1.0, 1.0,   1.0, 1.0, 0.0,
+        1.0, 0.0, 0.0,   1.0, 0.0, 1.0,   1.0, 1.0, 1.0,
+        // x0z
+        1.0, 0.0, 0.0,   0.0, 0.0, 0.0,   1.0, 0.0, 1.0,
+        0.0, 0.0, 0.0,   0.0, 0.0, 1.0,   1.0, 0.0, 1.0,
+        // x1z face
+        0.0, 1.0, 1.0,   0.0, 1.0, 0.0,   1.0, 1.0, 1.0,
+        1.0, 1.0, 1.0,   0.0, 1.0, 0.0,   1.0, 1.0, 0.0
+       ];
+       
+       this.cubeVertsUV = [
+        // xy0 face
+        0,0,1,1,0,1,
+        0,0,1,0,1,1,
+        // xy1
+        1,1,0,0,0,1,
+        1,0,0,0,1,1,
+        // 0yz
+        1,1,0,0,1,0,
+        0,1,0,0,1,1,
+        // 1yz
+        0,0,1,1,1,0,
+        0,0,0,1,1,1,
+        // x0z
+        1,0,0,0,1,1,
+        0,0,0,1,1,1,
+        // x1z face
+        0,1,0,0,1,1,
+        1,1,0,0,1,0
+       ];
    }
 
    render() {
@@ -47,45 +89,9 @@ class Cube {
     var rgba = this.color;
     gl.uniform1i(u_whichTexture, this.textureNum);
     gl.uniformMatrix4fv(u_ModelMatrix, false, this.matrix.elements);
-    var allVertsXYZ = [];
-    var allVertsUV = [];
-    
-    // xy0 face
     gl.uniform4f(u_FragColor, rgba[0], rgba[1], rgba[2], rgba[3]);
-    allVertsXYZ = allVertsXYZ.concat([0.0, 0.0, 0.0,   1.0, 1.0, 0.0,   0.0, 1.0, 0.0]);
-    allVertsUV = allVertsUV.concat([0,0,1,1,0,1]);
-    allVertsXYZ = allVertsXYZ.concat([0.0, 0.0, 0.0,   1.0, 0.0, 0.0,   1.0, 1.0, 0.0]);
-    allVertsUV = allVertsUV.concat([0,0,1,0,1,1]);
-    // xy1
-    gl.uniform4f(u_FragColor, rgba[0]*0.9, rgba[1]*0.9, rgba[2]*0.9, rgba[3]);
-    allVertsXYZ = allVertsXYZ.concat([1.0, 1.0, 1.0,   0.0, 0.0, 1.0,   0.0, 1.0, 1.0]);
-    allVertsUV = allVertsUV.concat([1,1,0,0,0,1]);
-    allVertsXYZ = allVertsXYZ.concat([1.0, 0.0, 1.0,   0.0, 0.0, 1.0,   1.0, 1.0, 1.0]);
-    allVertsUV = allVertsUV.concat([1,0,0,0,1,1]);
-    // 0yz
-    gl.uniform4f(u_FragColor, rgba[0]*0.8, rgba[1]*0.8, rgba[2]*0.8, rgba[3]);
-    allVertsXYZ = allVertsXYZ.concat([0.0, 1.0, 1.0,   0.0, 0.0, 0.0,   0.0, 1.0, 0.0]);
-    allVertsUV = allVertsUV.concat([1,1,0,0,1,0]);
-    allVertsXYZ = allVertsXYZ.concat([0.0, 0.0, 1.0,   0.0, 0.0, 0.0,   0.0, 1.0, 1.0]);
-    allVertsUV = allVertsUV.concat([0,1,0,0,1,1]);
-    // 1yz
-    gl.uniform4f(u_FragColor, rgba[0]*0.7, rgba[1]*0.7, rgba[2]*0.7, rgba[3]);
-    allVertsXYZ = allVertsXYZ.concat([1.0, 0.0, 0.0,   1.0, 1.0, 1.0,   1.0, 1.0, 0.0]);
-    allVertsUV = allVertsUV.concat( [0,0,1,1,1,0]);
-    allVertsXYZ = allVertsXYZ.concat([1.0, 0.0, 0.0,   1.0, 0.0, 1.0,   1.0, 1.0, 1.0]);
-    allVertsUV = allVertsUV.concat( [0,0,0,1,1,1]);
-    // x0z
-    gl.uniform4f(u_FragColor, rgba[0]*0.6, rgba[1]*0.6, rgba[2]*0.6, rgba[3]);
-    allVertsXYZ = allVertsXYZ.concat([1.0, 0.0, 0.0,   0.0, 0.0, 0.0,   1.0, 0.0, 1.0]);
-    allVertsUV = allVertsUV.concat([1,0,0,0,1,1]);
-    allVertsXYZ = allVertsXYZ.concat([0.0, 0.0, 0.0,   0.0, 0.0, 1.0,   1.0, 0.0, 1.0]);
-    allVertsUV = allVertsUV.concat( [0,0,0,1,1,1]);
-    // x1z face
-    gl.uniform4f(u_FragColor, rgba[0]*0.7, rgba[1]*0.7, rgba[2]*0.7, rgba[3]);
-    allVertsXYZ = allVertsXYZ.concat([0.0, 1.0, 1.0,   0.0, 1.0, 0.0,   1.0, 1.0, 1.0]);
-    allVertsUV = allVertsUV.concat([0,1,0,0,1,1]);
-    allVertsXYZ = allVertsXYZ.concat([1.0, 1.0, 1.0,   0.0, 1.0, 0.0,   1.0, 1.0, 0.0]);
-    allVertsUV = allVertsUV.concat( [1,1,0,0,1,0]);
-    drawTriangle3DUV(allVertsXYZ, allVertsUV);
+    drawTriangle3DUV(this.cubeVertsXYZ, this.cubeVertsUV);
    }
+   
+   
 }
